a5c4d0c329dac5e9b71c21785cd0db236845310d
DeLoSo <49134118+DeLoSoCode@users.noreply.github.com>
2023-12-10 15:02:22 +0800

    Update DeLoSoforYosys.py

--- method/DeLoSoforYosys/DeLoSoforYosys.py
+++ method/DeLoSoforYosys/DeLoSoforYosys.py
@@ -157,7 +157,7 @@ def extract_params(command):
     return params
 
 
-def optimizeGA(n_pop=50, n_gen=30, cxpb=0.5, mutpb=0.2, lambda_=10, mu=2,num_solutions=10):
+def optimizeGA(n_pop=50, n_gen=50, cxpb=0.5, mutpb=0.2, lambda_=10, mu=2,num_solutions=10):
     creator.create("FitnessMulti", base.Fitness, weights=(0.8, 0.2))
     creator.create('Individual', list, fitness=creator.FitnessMulti)
     toolbox = base.Toolbox()
